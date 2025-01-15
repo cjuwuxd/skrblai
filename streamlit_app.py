6c5a159c7474feeebd3bce64d14caf80e6b71077
cjuwuxd <cjuwuxd@gmail.com>
2025-01-15 17:22:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
-
 from PIL import Image
 import google.generativeai as genai
 import config
 import streamlit as st
 from streamlit_drawable_canvas import *
 import streamlit.components.v1 as components
+import time
+import threading
 
 genai.configure(api_key=config.api_key)  
 model = genai.GenerativeModel(model_name='gemini-1.5-flash')
@@ -29,6 +30,11 @@ canvas_result = st_canvas(
 
 canvas_result = canvas_result.image_data
 
+def defaultVariables():
+    config.uses = 2
+    config.lock = False
+    config.exampleQuestion = " "
+    config.history = " "
 
 def get_image_canvas():
     global canvas_result
@@ -55,11 +61,13 @@ def evaluate(image):
 
 def handle_usage_limit(image):
     global model
-    
+    st.write("Uses Left: " + str(config.uses))
     if config.uses <= 0 and config.lock == False:
         studentLock()
+        timer_threading.start()
+
     elif config.lock == True:
-        studentUnlock(question=config.exampleQuestion)
+        studentUnlock(question=config.exampleQuestion, image=image)
     else:
         evaluate(image)
         config.uses -= 1
@@ -75,21 +83,34 @@ def studentLock():
     config.exampleQuestion = question.text
     
 
-def studentUnlock(question):
+def studentUnlock(question, image):
     prompt_question = f"What is the answer to this question and compare it to mine {question}, if its correct just say TRUE"
-    answer = model.generate_content([prompt_question])
+    answer = model.generate_content([prompt_question, image])
     print(answer.text)
 
     if config.uses <= 0:
         if "TRUE" in answer.text:
             st.success("Correct!")
-            config.uses = 2
-            config.lock = False
-            config.exampleQuestion = " "
-            config.history = " "
+            defaultVariables()
+            timer_threading.stop()
         else:
+            st.write(question)
             st.error("That is incorrect. Please try again!")
 
+
+def startTimer(t):
+    while t: 
+        ph = st.empty()
+        mins, secs = divmod(t, 60)
+        timer = "{:02d}:{:02d}".format(mins,secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+        ph.metric("Refreshing Uses in: ", timer)
+    defaultVariables()
+    
+timer_threading = threading.Thread(target=startTimer, args=(300,))
+timer_threading.daemon = True
 
 user_input = st.text_input("Enter your input:")
 
@@ -113,5 +134,5 @@ if __name__ == "__main__":
     try:
         get_image_canvas()
     except TypeError:
-        print("Error")
+        print("Error:" + TypeError)
     
