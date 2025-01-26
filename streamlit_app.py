9b6b21c832f62f93c501bb972f1c00acadf4d18f
cjuwuxd <cjuwuxd@gmail.com>
2025-01-26 12:09:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import google.generativeai as genai
 import config
 import streamlit as st
@@ -6,6 +6,13 @@ from streamlit_drawable_canvas import *
 import streamlit.components.v1 as components
 import time
 import threading
+import numpy as np
+
+theme = {
+    "base": "dark",
+    "primaryColor": "#40da16",
+    "font": "monospace"
+}
 
 genai.configure(api_key=config.api_key)  
 model = genai.GenerativeModel(model_name='gemini-1.5-flash')
@@ -15,48 +22,66 @@ st.title("SKRBL.ai")
 
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color: ", "#ffffff")
+
 fill_color = "#000000"
-width = 700
+width = 800
 height = 500
 
-canvas_result = st_canvas(
-        stroke_width=stroke_width,
-        stroke_color=stroke_color,
-        fill_color=fill_color,
-        width=width,
-        height=height,
-        key="full_app"
-    )
+def SKRBL_main():
+    global canvas_result
+    canvas_result = st_canvas(
+            stroke_width=stroke_width,
+            stroke_color=stroke_color,
+            fill_color=fill_color,
+            width=width,
+            height=height,
+            key="full_app"
+        )
 
-canvas_result = canvas_result.image_data
+    canvas_result = canvas_result.image_data
+    canvas_result = Image.fromarray(canvas_result)
+    config.canvasImage = canvas_result
+    
+
+def SKRBL_cam():
+    cam_mode = st.checkbox("Enable Camera")
+    picture = st.camera_input("SKRBL your question!" , disabled=not cam_mode)
+
+    if picture is not None:
+        img = Image.open(picture)
+        img_array = np.array(img)
+        img_array = Image.fromarray(img_array)
+        config.canvasImage = img_array
 
 def defaultVariables():
-    config.uses = 15
+    config.uses = 2
     config.lock = False
     config.exampleQuestion = " "
     config.history = " "
 
-def get_image_canvas():
-    global canvas_result
-    
+def get_image_canvas(canvas_result):
     canvas_result = Image.fromarray(canvas_result)
-    return canvas_result
+    handle_usage_limit(canvas_result)
 
 
 def evaluate(image):
-    global model
-    
-    prompt = f"Analyze the image and solve it mathematically and keep the answer short and still explains it, and explain it in a step by step process. {user_input}"
+    if config.mode == 1:
+        global model
+        
+        prompt = f"Analyze the image and solve it mathematically and keep the answer short and still explains it, and explain it in a step by step process. {user_input}"
 
-    response = model.generate_content([prompt, image])
-    popup = f"<script> alert({response.text})</script>"
-    components.html(popup,height=0,width=0)
-    st.success(response.text)
-    
-    print("Uses Remaining: "  + str(config.uses) )
-    print(response.text)
-    config.history = response.text
-    return response.text
+        response = model.generate_content([prompt, image])
+        popup = f"<script> alert({response.text})</script>"
+        components.html(popup,height=0,width=0)
+        st.success(response.text)
+        
+        print("Uses Remaining: "  + str(config.uses) )
+        print(response.text)
+        config.history = response.text
+        return response.text
+
+    elif config.mode == 2:
+        pass
 
 
 def handle_usage_limit(image):
@@ -120,19 +145,28 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     if st.button("Calculate"):
-        
-        handle_usage_limit(get_image_canvas())
+        handle_usage_limit(config.canvasImage)
         
         
 
 
+page_names_to_func = {
+        "SKRBL Mode": SKRBL_main,
+        "Capture Mode": SKRBL_cam
+    }
+
+
+pages = st.sidebar.selectbox("Choose Mode",page_names_to_func.keys())    
+page_names_to_func[pages]()
    
 if __name__ == "__main__":
     #root = tk.Tk()
     #app = Calculator(root)
     #root.mainloop()
-    try:
-        get_image_canvas()
-    except TypeError:
-        pass
+    # try:
+        
+    #     get_image_canvas()
+    # except TypeError:
+    #     print("Error:" + TypeError)
     
+    pass
