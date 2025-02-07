d05409e99ab09d48d0f74f21acd1a6d5a06059e5
cjuwuxd <cjuwuxd@gmail.com>
2025-02-07 18:55:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def evaluate(image):
         response = model.generate_content([prompt, image])
         #popup = f"<script> alert({response.text})</script>"
         #components.html(popup,height=0,width=0)
-        st.success(response.text)
+        st.text_area(response.text)
         
         print("Uses Remaining: "  + str(config.uses) )
         print(response.text)
