695a2d706c291e53f15e4efba8e9e9fe2f05a88b
cjuwuxd <cjuwuxd@gmail.com>
2025-02-07 20:46:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ theme = {
     "primaryColor": "#40da16",
     "font": "monospace"
 }
-width = 1920
-height = 1080
+
+height = 720
 layout = "wide"
 genai.configure(api_key=config.api_key)  
 model = genai.GenerativeModel(model_name='gemini-2.0-flash')
